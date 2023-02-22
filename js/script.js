af0d56c4fb4c984a3b4d604bb36ad4e5377e0649
martinapiglio <martina.piglio@gmail.com>
2023-02-22 15:23:40 +0100

    file scaffolding

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+1 - chiedere all'utente il numero di chilometri che vuole percorrere. 
+2 -chiedere all'utente l'età.
+
+3 - calcolare il prezzo totale del viaggio:
+    a - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    b - va applicato uno sconto del 20% per i minorenni
+    c - va applicato uno sconto del 40% per gli over 65.
+
+4 - Prezzo finale con 2 decimali + in forma umana.
+*/
+
